c7144e8abf6fcc0472b3fa96f568a0e058dffa09
Shishira Bhat O <shishirabhato2@gmail.com>
2022-11-30 12:14:09 +0530

    Updated commands and project report

--- Commands/join operations.sql
+++ Commands/join operations.sql
@@ -14,5 +14,9 @@ SELECT player_397.Player_Name,team_397.Team_Name FROM player_397 LEFT JOIN team_
 
  SELECT team_397.Team_Name,revenue_397.Source_name FROM revenue_397 RIGHT JOIN team_397 ON revenue_397.Team_ID = team_397.Team_ID;
 
+-- Nested Join
+
+SELECT player_397.Player_Name,Team_397.Team_Name,Competition_Name FROM participates_397 NATURAL JOIN competition_397 JOIN team_397 ON team_397.Team_ID = participates_397.Team_ID JOIN player_397 ON player_397.Team_ID = team_397.Team_ID;
+
 
 
